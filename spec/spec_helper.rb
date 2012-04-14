1c38cca5b455eef6cd5cc7f5510dd7ecc8f13c78
Brandon Turner <bt@brandonturner.net>
2012-04-13 23:40:54 -0500

    Add tests for crowd token key lookup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,15 +6,23 @@ Bundler.setup(:default, :test)
 
 require 'rspec'
 require 'rr'
+require 'ostruct'
 require 'action_controller'
 require 'mongoid'
 require 'devise'
 require 'devise_crowd'
+require 'simple_crowd/mock_client'
+
 Devise.setup do |config|
+  config.apply_schema = false
   require 'devise/orm/mongoid'
   config.crowd_enabled = true
+  config.case_insensitive_keys = [ ]
+  config.use_salt_as_remember_token = true
+  config.reset_password_within = 2.hours
 end
 require 'mock/user'
+require 'rails_app/config/environment'
 
 
 # Requires supporting files with custom matchers and macros, etc,
@@ -23,4 +31,19 @@ Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
 RSpec.configure do |config|
   config.mock_with :rr
+
+  config.before :each do
+    @mock_crowd_client = SimpleCrowd::MockClient.new
+    stub(SimpleCrowd::Client).new {@mock_crowd_client}
+    SimpleCrowd::MockClient.reset
+  end
+end
+
+# Create a very simple Warden::Manager mock that contains an empty session
+# hash.  The DeviseCrowd strategy saves session data directly to the session
+# hash which we stub here.
+def warden_manager
+  mock_warden = OpenStruct.new
+  mock_warden.session = mock_warden.raw_session = {}
+  mock_warden
 end
--- spec/strategy_spec.rb
+++ spec/strategy_spec.rb
@@ -4,6 +4,9 @@ module Devise::Strategies
 
   describe CrowdAuthenticatable do
 
+    CROWD_USERNAME = 'gcostanza'
+    CROWD_TOKEN = '1234567890abcdefghijklmno'
+
     # TODO: This should not be needed.  This throws an exception including
     # Rails.application.routes.url_helpers the first time it is called.
     before(:all) do
@@ -13,26 +16,50 @@ module Devise::Strategies
     before(:each) do
       Devise.add_mapping(:mock_users, :class_name => Devise::Mock::User)
       @model = Devise::Mock::User.new(:id => 555)
-      stub(Devise::Mock::User).find_for_authentication({:id => '555'}){@model}
     end
 
     def strategy(uri, cookies={})
-      CrowdAuthenticatable.new(Rack::MockRequest.env_for(uri, 'HTTP_COOKIE'=>cookies.to_query), :mock_user)
+      env = Rack::MockRequest.env_for(uri, 'HTTP_COOKIE'=>cookies.to_query)
+      env['warden'] = warden_manager
+      CrowdAuthenticatable.new(env, :mock_user)
     end
 
     context "with a crowd token cookie" do
       before(:each) do
-        @strategy = strategy("http://example.com/foobar", 'crowd.token_key' => 555)
+        @strategy = strategy("http://example.com/foobar", 'crowd.token_key' => CROWD_TOKEN)
       end
 
       it "is valid for crowd authentication" do
         @strategy.should be_valid
       end
+
+      it "authenticates the crowd token" do
+        mock(@mock_crowd_client).is_valid_user_token?(CROWD_TOKEN) {true}
+        mock(@mock_crowd_client).find_username_by_token(CROWD_TOKEN) {CROWD_USERNAME}
+        mock(Devise::Mock::User).find_for_authentication({'crowd_username' => CROWD_USERNAME}){@model}
+        #mock.proxy(@strategy).success!(@model)
+        @strategy.authenticate!
+        @strategy.result.should == :success
+      end
+
+      it "rejects an invalid crowd token" do
+        mock(@mock_crowd_client).is_valid_user_token?(CROWD_TOKEN) {false}
+        @strategy.authenticate!
+        @strategy.result.should == :failure
+      end
+
+      it "rejects an unknown crowd username" do
+        mock(@mock_crowd_client).is_valid_user_token?(CROWD_TOKEN) {true}
+        mock(@mock_crowd_client).find_username_by_token(CROWD_TOKEN) {'foobar'}
+        mock(Devise::Mock::User).find_for_authentication({'crowd_username' => 'foobar'}){nil}
+        @strategy.authenticate!
+        @strategy.result.should == :failure
+      end
     end
 
     context "with a crowd token param" do
       before(:each) do
-        @strategy = strategy("http://example.com/foobar?crowd.token_key=555")
+        @strategy = strategy("http://example.com/foobar?crowd.token_key=#{CROWD_TOKEN}")
       end
 
       it "is valid for crowd authentication" do
